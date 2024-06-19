6bf44140f14ea185cbed42e7b333b34af82ebe15
hailetotawmeta <haile.kassa.mamo@gmail.com>
2024-06-20 01:35:57 +0400

    welcome page added

--- src/components/welcomeMessage.tsx
+++ src/components/welcomeMessage.tsx
@@ -6,8 +6,31 @@ const WelcomeMessage: React.FC = () => {
   return (
     <div className="p-8">
       <div className="bg-white bg-opacity-70 p-8 shadow-md rounded-lg">
-        <h2 className="text-black text-2xl font-bold">Welcome to the Church</h2>
-        <p className="text-gray-800">Introductory message goes here...</p>
+        <h2 className="text-black text-2xl font-bold">
+          Welcome Debre Sina Kidan Mehret Ethiopian Orthodox Church
+        </h2>
+        <p className="text-gray-800">
+          The Debre Sina Kidan Mehret Ethiopian Orthodox Church began its
+          journey in the year 2000, supported by the St. Mary Ethiopian Orthodox
+          Church in Toronto. This collaboration marked the beginning of a
+          dedicated congregation committed to fostering the spiritual and
+          cultural traditions of the Ethiopian Orthodox Tewahedo faith in
+          Canada. In 2016, a significant milestone was achieved when the church
+          acquired its own property at 1248 Kramp Road, Breslau, Ontario. This
+          acquisition provided a permanent home for the congregation, allowing
+          it to grow and thrive within its own space. Since then, Debre Sina
+          Kidan Mehret Ethiopian Orthodox Church has been a vibrant center of
+          worship and community activities. The church offers weekly services,
+          including the Holy Communion, Holy Table, and spiritual counseling,
+          which are cornerstones of the Ethiopian Orthodox faith. The church
+          also emphasizes the importance of youth and children's spiritual
+          development, offering dedicated services and programs to nurture the
+          younger members of the congregation. Through the commitment of its
+          members and the grace of God, Debre Sina Kidan Mehret Ethiopian
+          Orthodox Church continues to serve as a beacon of faith and community
+          for Ethiopians in southwestern Ontario under the Holy Synod
+          Archdiocese of Eastern Canada.
+        </p>
       </div>
     </div>
   );
